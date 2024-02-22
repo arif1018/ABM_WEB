436e655e65c496b3aad6c53df609ccfada573996
arif1018 <arif.razzaque.khi@gmail.com>
2024-02-22 17:55:32 +0500

    Updated 22022024 0555PM Office PC

--- frontend/src/services/GlobalVariables.js
+++ frontend/src/services/GlobalVariables.js
@@ -1,5 +1,5 @@
-global.SET_API_URL = 'https://serverwebapp.lifconsteel.com/'
+global.SET_API_URL = 'https://server.whdevs.com/'
 global.COMPANY_NAME = 'ABM BUSINESS MACHINE'
 
-// global.SET_API_URL = 'http://localhost:5003/'
+// global.SET_API_URL = 'http://localhost:5002/'
 // global.COMPANY_NAME = 'WORLD HOME DEVELOPMENT'
